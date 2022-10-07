05d707a0efc7683e3536f22698b396f503329d91
VivekGujale <vdgujale007@.com>
2022-10-07 16:39:49 +0530

    first commit

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,4 +1,21 @@
 public class AddressBookMain {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private int phoneNumber;
+
+    public void setFirstName() {
+
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
     }
---
+++ src/AddressBookMain/AddressbookMain.java
@@ -0,0 +1,7 @@
+package AddressBookMain;
+
+public class AddressbookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+    }
+}
